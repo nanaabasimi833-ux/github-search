025e0983e7082e28344e2ca4d2fddef08a783df7
Nana Abasimi <221759464+nanaabasimi833-ux@users.noreply.github.com>
2026-08-22 17:13:25 +0100

    Handle profile changes from the URL

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,10 +1,40 @@
 import { useParams } from "react-router-dom";
-
+import { useEffect, useState } from "react";
+import type { GitHubUserDetail } from "../types";
 
  const ProfilePage = () => {
   const {login} = useParams()
+  const [user, setUser] = useState<GitHubUserDetail | null>(null)
+
+  useEffect(()=>{
+    if(!login){return}
+    
+    const loadUser = async()=>{
+      try{
+
+        const response = await fetch(`https://api.github.com/users/${encodeURIComponent(login)}`)
+
+        const data = await response.json()
+        setUser(data)
+    }catch{
+      console.error();
+      /*  
+      Add error handling  
+      
+      */
+      
+    }
+    }
+    
+    loadUser()
+
+
+  },[login])
+
   return ( 
-    <div>{login}</div>
+    <div>
+       <p> {user?.login} </p>
+    </div>
   )
   }
 
